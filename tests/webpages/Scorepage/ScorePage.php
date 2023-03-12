b5b4578b1a9d5ecb7ee28d3139f15db08579cb72
GPS921 <gpsc7c@umsl.edu>
2023-03-11 21:39:18 -0600

    Scoreboard added I hereby delegate CSS to people who aren't bad at it

---
+++ tests/webpages/Scorepage/ScorePage.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>FRACTION RUNNER Scoreboard</title>
+        <link rel="stylesheet" href="ScoreStyle.css" type="text/css">
+    </head>
+    <body>
+        <header>
+            <h1>FRACTION RUNNER</h1>
+        </header>
+
+        <table class="scoretable">
+            <tableh>
+                <tr>
+                    <th>Name</th>
+                    <th>Points</th>
+                </tr>
+            </tableh>
+            <tableb>
+                <tr>
+                    <td>NAME</td>
+                    <td>6000</td>
+                </tr>
+                <tr class="active-row">
+                    <td>OTHER</td>
+                    <td>5150</td>
+                </tr>
+            </tableb>
+        </table>
+        <footer>
+            <p>
+                All rights reserved
+            </p>
+        </footer>
+    </body>
+</html>
