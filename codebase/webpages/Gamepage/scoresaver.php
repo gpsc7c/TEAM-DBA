63c49856f21fb1a242565eb5901168e500f3672d
GPS921 <gpsc7c@umsl.edu>
2023-05-05 13:17:41 -0500

    gamepage now has saver pages

---
+++ codebase/webpages/Gamepage/scoresaver.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+include '../Scorepage/scoreDatabaseFunctions.php';
+$ranks = new scoreDatabaseFunctions();
+$userscore = $_COOKIE['userscore'];
+echo $userscore;
+$name = $_SESSION['username'];
+
+try{
+    $sql = $ranks->setUserScore($ranks->dbconn, "test", $userscore, "0909");
+}catch(mysqli_sql_exception $e){
+    
+}
+
+?>
